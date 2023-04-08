9bb182dcbf3ef5528d23dce5bca29635cb9fdd80
sd <dasarwarshubham1@gmail.com>
2023-04-08 07:33:53 +0530

    hero image added and button color changed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Weather from "./components/Weather";
 import { getWeatherData } from "./services/weather-api";
 import { Spinner } from "react-bootstrap";
 
+import { ReactComponent as WeatherIcon } from "./assets/weather-icon.svg";
+
 function App() {
   const [weatherData, setWeatherData] = useState({});
   const [loading, setLoading] = useState(false);
@@ -23,22 +25,28 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <h1 className="text-center my-4">Weather App</h1>
-      <Form handleSearch={handleSearch} />
-      {loading && (
-        <div className="text-center my-3">
-          <Spinner animation="border" role="status"></Spinner>
-        </div>
-      )}
-      {error && (
-        <div className="alert alert-danger my-3" role="alert">
-          {error}
-        </div>
-      )}
-      {Object.keys(weatherData).length !== 0 && (
-        <Weather weatherData={weatherData} />
-      )}
+    <div className="container vh-100 d-flex justify-content-center align-items-center">
+      <div>
+        <WeatherIcon
+          className="weather-icon"
+          style={{ maxHeight: "150px", width: "100%" }}
+        />
+        <h1 className="text-center my-4">Weather App</h1>
+        <Form handleSearch={handleSearch} />
+        {loading && (
+          <div className="text-center my-3">
+            <Spinner animation="border" role="status"></Spinner>
+          </div>
+        )}
+        {error && (
+          <div className="alert alert-danger my-3" role="alert">
+            {error}
+          </div>
+        )}
+        {Object.keys(weatherData).length !== 0 && (
+          <Weather weatherData={weatherData} />
+        )}
+      </div>
     </div>
   );
 }
